3e7eff172e27633d591aa4f5656341b7da2d4154
Edoardo Odorico <edoardo.odorico@zalando.de>
2023-06-19 16:36:27 +0200

    fix: onNext being called when story ends

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -71,7 +71,7 @@ export default function () {
     if (e.key === "ArrowLeft") {
       previous();
     } else if (e.key === "ArrowRight") {
-      next();
+      next(true);
     }
   };
 
@@ -86,14 +86,14 @@ export default function () {
   };
 
   const previous = () => {
-    if(onPrevious != undefined){
+    if (onPrevious != undefined) {
       onPrevious();
     }
     setCurrentIdWrapper((prev) => (prev > 0 ? prev - 1 : prev));
   };
 
-  const next = () => {
-    if(onNext != undefined){
+  const next = (isSkippedByUser?: boolean) => {
+    if (onNext != undefined && isSkippedByUser) {
       onNext();
     }
     // Check if component is mounted - for issue #130 (https://github.com/mohitk05/react-insta-stories/issues/130)
@@ -137,7 +137,7 @@ export default function () {
       if (pause) {
         toggleState("play");
       } else {
-        type === "next" ? next() : previous();
+        type === "next" ? next(true) : previous();
       }
     };
 
